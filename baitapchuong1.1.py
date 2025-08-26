139a0efdc81a4d4a857d21432db188bf11c58617
DTH235731_PhoBaoPhong <taphongpho@gmail.com>
2025-08-26 10:33:09 +0700

    Add files via upload

---
+++ baitapchuong1.1.py
@@ -0,0 +1,4 @@
+print('Hello World!')
+print('Albert Einstein đã từng nói: một người không bao giờ mắc sai lầm không bao giờ thử bất kỳ điều gì mới.')
+
+
---
+++ baitapchuong1.8.py
@@ -0,0 +1,14 @@
+#module1.py
+question = 'what is the meaning of Life, the Umiverse, and Everything?'
+answer =  42
+
+#module2.py
+question1 =  'What is your quest?'
+answer1 = 'To seek the holy grail.'
+
+import module1
+import module2
+print(module1.question)
+print(module2.question)
+print(module1.answer)
+print(module2.answer)
